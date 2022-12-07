6a316ab85f31a03c73aa44a77f3129377cb4e402
Nick Radford <nick@nicholasradford.com>
2022-12-06 22:16:44 -0800

    vite vite!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.tsx"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
